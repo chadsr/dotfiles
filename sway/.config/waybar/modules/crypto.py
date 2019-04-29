2d3dcdef7421c0d313a35411d4c731f51a35f219
Ross Chadwick <ross@rchadwick.co.uk>
2019-04-30 00:23:35 +0200

    Added functioning cryptocurrency bar

--- sway/.config/waybar/modules/crypto.py
+++ sway/.config/waybar/modules/crypto.py
@@ -4,28 +4,69 @@ import configparser
 import sys
 import os
 import requests
+import json
 from decimal import Decimal
+
+API_URL = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
 
 config = configparser.ConfigParser()
 abs_dir = os.path.dirname(os.path.abspath(__file__)) # Get the absolute path of this script
 with open(f'{abs_dir}/crypto.ini', 'r', encoding='utf-8') as f:
 	config.read_file(f)
 
-coins = [x for x in config.sections() if x != 'general'] # All coin sections from the config
-base_currency = config['general']['base_currency'] # The fiat currency used in the trading pair
-params = {'convert': base_currency}
+coins = [section for section in config.sections() if section != 'general'] # Any section that isn't general, is a coin
+currency = config['general']['currency'].upper() # The fiat currency used in the trading pair
+currency_symbol = config['general']['currency_symbol']
+
+params = {
+	'convert': currency.upper(),
+	'symbol': ','.join(coin.upper() for coin in coins)
+}
+
 headers = {'X-CMC_PRO_API_KEY': config['general']['api_key']}
+
+# Request the chosen price pairs
+response = requests.get(API_URL, params=params, headers=headers, timeout=2)
+if response.status_code != 200:
+	print("Coinmarketcap API returned non 200 response:\n", response.content)
+	sys.exit(1)
+
+# Get a list of the chosen display options
+display_options = config['general']['display'].split(',')
+
+# For each coin, write to stdout, formatted according to the chosen settings
+
+output_obj = {
+	'text': '',
+	'tooltip': "Cryptocurrency metrics from Coinmarketcap:\n",
+	'class': 'crypto'
+}
 
 for coin in coins:
 	icon = config[coin]['icon']
-	json = requests.get(f'https://pro-api.coinmarketcap.com/v1/{currency}', params=params, headers=headers).json()[0]
-	local_price = round(Decimal(json[f'price_{base_currency.lower()}']), 2)
-	change_24 = float(json['percent_change_24h'])
+	
+	# Extract the object relevant to our coin/currency pair
+	pair_info = response.json()['data'][coin.upper()]['quote'][currency]
 
-	display_opt = config['general']['display']
-	if display_opt == 'both' or display_opt == None:
-		sys.stdout.write(f'{icon} {local_price}/{change_24:+}% ')
-	elif display_opt == 'percentage':
-		sys.stdout.write(f'{icon} {change_24:+}%')
-	elif display_opt == 'price':
-		sys.stdout.write(f'{icon} {local_price}')
+	output = f'{icon}'
+	# Shows price by default
+	if 'price' in display_options or not display_options:
+		current_price = round(Decimal(pair_info['price']), 2)
+		output += f'{currency_symbol}{current_price} '
+	if 'volume' in display_options:
+		percentage_change = round(Decimal(pair_info['volume_24']), 2)
+		output += f'24h:{currency_symbol}{percentage_change:+} '
+	if 'change24' in display_options:
+		percentage_change = round(Decimal(pair_info['percent_change_24h']), 2)
+		output += f'24h:{percentage_change:+}% '
+	if 'change1h' in display_options:
+		percentage_change = round(Decimal(pair_info['percent_change_1h']), 2)
+		output += f'1h:{percentage_change:+}% '
+	if 'change7d' in display_options:
+		percentage_change = round(Decimal(pair_info['percent_change_7d']), 2)
+		output += f'7d:{percentage_change:+}% '
+
+	output_obj['text'] += output
+	output_obj['tooltip'] += output
+
+sys.stdout.write(json.dumps(output_obj))
