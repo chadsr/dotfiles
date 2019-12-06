b0388d4e2d4b62d348fd53687f20ab9886a1e795
Ross Chadwick <git@ross.ch>
2019-12-06 19:03:39 +0100

    Symlinking system configs is a bad idea, revert to just overwriting with cp + fix deps

--- stow_all.sh
+++ stow_all.sh
@@ -18,12 +18,10 @@ if [ $1 == "laptop" ] ; then
     prompt "This will remove existing system settings"
     echo "Checking if tpacpi-bat is installed"
     yay -S --needed tpacpi-bat
-    sudo rm -rf /etc/default/tlp
-    sudo stow -v --target="/" laptop_system
+    sudo cp -Rv laptop_system/* /
 elif [ $1 == "workstation" ] ; then
     prompt "This will remove existing system settings"
-    sudo rm -rf /etc/default/tlp
-    sudo stow -v --target="/" workstation_system
+    sudo cp -Rv workstation_system/* /
 else 
     echo "$0: first argument must be 'laptop' or 'workstation'"
     exit 1
@@ -47,7 +45,7 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 yay -S --needed zsh thefuck
 
 echo "Checking for Sway dependencies to install"
-yay -S --needed sway light waybar libappindicator-gtk3 dex rofi otf-font-awesome python python-requests networkmanager-dmenu slurp grim swayshot swaylock-blur-git mako redshift-wlr-gamma-control-git gtk-engines alacritty
+yay -S --needed sway ranger shotwell light waybar libappindicator-gtk3 dex rofi otf-font-awesome python python-requests networkmanager-dmenu slurp grim swayshot swaylock-blur-git mako redshift-wlr-gamma-control-git gtk-engines alacritty
 
 echo "Setting up user directory configs"
 stow -v firefox
