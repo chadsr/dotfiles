c25fbe01b0fe5ae478d23dbb4b4ce546ca0ca473
Ross Chadwick <git@ross.ch>
2019-12-06 15:45:58 +0100

    Improved scripts

--- stow_all.sh
+++ stow_all.sh
@@ -12,6 +12,8 @@ prompt () {
     esac
 }
 
+./stow_setup.sh
+
 if [ $1 == "laptop" ] ; then
     prompt "This will remove existing system settings"
     echo "Checking if tpacpi-bat is installed"
@@ -29,16 +31,25 @@ fi
 
 echo "Getting git submodules"
 git submodule update --init --recursive
+cd git_themes/PUNK
+git checkout Ultimate-Punk-Complete-Desktop
+cd ../sweet_dark
+git checkout nova
+cd ../../
+
+echo "Copying themes from git repo to dotfiles locations"
+cp -Rv ./git_themes/PUNK/PUNK-Cyan-Cursor ./sway/.icons/
+cp -Rv ./git_themes/PUNK/Ultimate-Punk-Suru++ ./sway/.icons/
+cp -Rv ./git_themes/sweet_dark ./sway/.themes/
 
 echo "Checking for ZSH dependencies to install"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"x
 yay -S --needed zsh thefuck
 
 echo "Checking for Sway dependencies to install"
-yay -S --needed sway light waybar libappindicator-gtk3 dex rofi otf-font-awesome python python-requests networkmanager-dmenu slurp grim swayshot swaylock-blur-git mako redshift-wlr-gamma-control-git sweet-theme-git gtk-engines alacritty suru-plus-git
+yay -S --needed sway light waybar libappindicator-gtk3 dex rofi otf-font-awesome python python-requests networkmanager-dmenu slurp grim swayshot swaylock-blur-git mako redshift-wlr-gamma-control-git gtk-engines alacritty
 
 echo "Setting up user directory configs"
-source ${HOME}/.stowrc
 stow -v firefox
 stow -v zsh
 stow -v sway
