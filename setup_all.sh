c81f60f57b2c1426f9bb068b7e4ed22997e83505
Ross <git@ross.ch>
2024-03-12 21:46:07 +0100

    setup: refactoring

--- setup_all.sh
+++ setup_all.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
-BASE_PATH=$PWD
-DATA_PATH="$BASE_PATH"/data
-SYSTEM_CONFIG="$BASE_PATH"/system
-GIT_SUBMODULES="$BASE_PATH"/.git_submodules
-GPG_PRIMARY_KEY=0x2B7340DB13C85766
-GPG_ENCRYPTION_SUBKEY=0x79C70BBE4865D828
+base_path=$PWD
+data_path="$base_path"/data
+system_config_path="$base_path"/system
+git_submodule_path="$base_path"/.git_submodules
 
-laptop_name="laptop"
-desktop_name="workstation"
+gpg_primary_key=0x2B7340DB13C85766
+gpg_encryption_subkey=0x79C70BBE4865D828
+
+laptop_hostname="thinky"
+desktop_hostname="shifty"
+current_hostname=$(hostname)
 
 exit_setup() {
     rv=$?
-    echo "Exiting setup..."
+    printf "\n\nExiting setup...\n"
     exit $rv
 }
 
@@ -22,7 +24,7 @@ trap 'exit_setup' ERR INT
 script_name=$(basename "$0")
 
 help() {
-    printf "Sets up system packages and configurations.\n\nUsage: %s <laptop|workstaton>\n" "$script_name"
+    printf "Sets up system packages and configurations.\n\nUsage: %s <%s|%s>\n" "$script_name" "$laptop_hostname" "$desktop_hostname"
 }
 
 prompt_exit() {
@@ -37,14 +39,17 @@ prompt_exit() {
     esac
 }
 
-if [ "$#" -ne 1 ]; then
-    help
-    exit 0
-fi
-
-if [[ "$1" != "$laptop_name" ]] && [[ "$1" != "$desktop_name" ]]; then
-    help
-    exit 1
+if [[ "$current_hostname" != "$laptop_hostname" ]] && [[ "$current_hostname" != "$desktop_hostname" ]]; then
+    if [[ "$1" != "$laptop_hostname" ]] && [[ "$1" != "$desktop_hostname" ]]; then
+        echo "Unrecognised hostname! Please provide a valid one."
+        help
+        exit 1
+    else
+        echo "Unrecognised hostname. Falling back to provided parameter: \"${1}\"."
+        current_hostname="$1"
+    fi
+else
+    echo "Using current hostname \"${current_hostname}\" for configuration."
 fi
 
 prompt_exit "This script will remove/change existing system settings."
@@ -87,15 +92,34 @@ gpg_ssh_agent() {
 }
 
 gpg_decrypt_file() {
-    gpg -v --local-user "$GPG_ENCRYPTION_SUBKEY" --armor --decrypt --yes --output "${2}" "${1}"
+    gpg -v --local-user "$gpg_encryption_subkey" --armor --decrypt --yes --output "${2}" "${1}"
 }
 
 gpg_list_dir() {
-    gpgtar -v --list-archive --gpg-args "--local-user ${GPG_ENCRYPTION_SUBKEY}" "${1}"
+    gpgtar -v --list-archive --gpg-args "--local-user ${gpg_encryption_subkey}" "${1}"
 }
 
 gpg_decrypt_dir() {
-    gpgtar -v --gpg-args "--local-user ${GPG_ENCRYPTION_SUBKEY}" --decrypt --yes --directory "${2}" "${1}"
+    gpgtar -v --gpg-args "--local-user ${gpg_encryption_subkey}" --decrypt --yes --directory "${2}" "${1}"
+}
+
+line_exists() {
+    case $(
+        grep -Fxq "${1}" "${2}" >/dev/null
+        echo $?
+    ) in
+    0)
+        # Found
+        return 0
+        ;;
+    1)
+        # Not found
+        return 1
+        ;;
+    *)
+        exit 1
+        ;;
+    esac
 }
 
 stow -t ~/ stow || {
@@ -164,6 +188,16 @@ yay -Syu || {
     exit 1
 }
 
+declare -a old_pkgs=(
+    swaylock swaylock-blur pipewire-media-session pipewire-pulseaudio pipewire-pulseaudio-git pulseaudio-equalizer pulseaudio-lirc pulseaudio-zeroconf pulseaudio pulseaudio-bluetooth redshift-wayland-git birdtray alacritty-colorscheme ly vscodium-bin vscodium-bin-features vscodium-bin-marketplace
+)
+echo "Checking for old packages to remove"
+for old_pkg in "${old_pkgs[@]}"; do
+    yay -R --noconfirm "$old_pkg" || {
+        :
+    }
+done
+
 echo "Installing script dependencies"
 yay_install git curl wget make stow gnupg pcsclite ccid inkscape xorg-xcursorgen wayland nano rustup sccache pkg-config meson ninja
 
@@ -175,7 +209,7 @@ rustup default stable || {
 echo "Checking for ZSH dependencies to install"
 yay_install zsh thefuck ttf-meslo-nerd-font-powerlevel10k
 
-read -p "Do you need to install ohmyzsh? ($GIT_SUBMODULES/ohmyzsh/tools/install.sh) (y/N)?" -n 1 -r
+read -p "Do you need to install ohmyzsh? (${git_submodule_path}/ohmyzsh/tools/install.sh) (y/N)?" -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     read -r -p "Press enter once the the installation is finished... [enter]"
@@ -195,7 +229,7 @@ stow -v gpg || {
 }
 
 # gpg-agent.conf doesn't support ENVs so replace variable here
-envsubst <"$DATA_PATH"/gpg/gpg-agent.conf >"$BASE_PATH"/gpg/.gnupg/gpg-agent.conf
+envsubst <"$data_path"/gpg/gpg-agent.conf >"$base_path"/gpg/.gnupg/gpg-agent.conf
 
 echo "Setting up GPG/SSH"
 
@@ -211,7 +245,7 @@ sudo systemctl start pcscd.socket || {
     exit 1
 }
 
-cp -f "$BASE_PATH"/data/ssh/yk.pub ~/.ssh || {
+cp -f "$base_path"/data/ssh/yk.pub ~/.ssh || {
     echo "failed to copy ssh pubkey"
     exit 1
 }
@@ -221,19 +255,19 @@ systemctl --user restart gpg-agent || {
     exit 1
 }
 
-gpg --import "$BASE_PATH"/data/gpg/2B7340DB13C85766.asc || {
+gpg --import "$base_path"/data/gpg/2B7340DB13C85766.asc || {
     echo "failed to import GPG pubkey"
     exit 1
 }
 
-gpg --tofu-policy good "$GPG_PRIMARY_KEY" || {
+gpg --tofu-policy good "$gpg_primary_key" || {
     echo "failed to set gpg tofu policy"
     exit 1
 }
 
 # Check if certain submodules get updated, so we don't build them uneccessarily
 hackneyed_updated=false
-hackneyed_hash_old=$(git -C "$GIT_SUBMODULES"/hackneyed-cursor rev-parse --short HEAD)
+hackneyed_hash_old=$(git -C "$git_submodule_path"/hackneyed-cursor rev-parse --short HEAD)
 
 git submodule update --progress --init --force --recursive --remote || {
     echo "failed to update git submodules"
@@ -250,19 +284,19 @@ git submodule foreach --recursive git reset --hard || {
     exit 1
 }
 
-hackneyed_hash_new=$(git -C "$GIT_SUBMODULES"/hackneyed-cursor rev-parse --short HEAD)
+hackneyed_hash_new=$(git -C "$git_submodule_path"/hackneyed-cursor rev-parse --short HEAD)
 if [[ "$hackneyed_hash_old" != "$hackneyed_hash_new" ]]; then
     hackneyed_updated=true
     echo "hackneyed-cursor has been updated"
 fi
 
 echo "Decrypting ./data files"
-gpg_decrypt_file "$DATA_PATH"/ssh/config.asc.gpg "$BASE_PATH"/ssh/.ssh/config
-gpg_decrypt_file "$DATA_PATH"/xdg/mimeapps.list.asc.gpg "$BASE_PATH"/xdg/.config/mimeapps.list
-gpg_decrypt_file "$DATA_PATH"/tidal-hifi/config.json.asc.gpg "$BASE_PATH"/tidal-hifi/.config/tidal-hifi/config.json
-gpg_decrypt_file "$DATA_PATH"/gallery-dl/config.json.asc.gpg "$BASE_PATH"/gallery-dl/.config/gallery-dl/config.json
-gpg_decrypt_file "$DATA_PATH"/waybar/crypto/config.ini.asc.gpg "$BASE_PATH"/sway/.config/waybar/modules/crypto/config.ini
-gpg_decrypt_file "$DATA_PATH"/gtk/bookmarks.asc.gpg "$BASE_PATH"/gtk/.config/gtk-3.0/bookmarks
+gpg_decrypt_file "$data_path"/ssh/config.asc.gpg "$base_path"/ssh/.ssh/config
+gpg_decrypt_file "$data_path"/xdg/mimeapps.list.asc.gpg "$base_path"/xdg/.config/mimeapps.list
+gpg_decrypt_file "$data_path"/tidal-hifi/config.json.asc.gpg "$base_path"/tidal-hifi/.config/tidal-hifi/config.json
+gpg_decrypt_file "$data_path"/gallery-dl/config.json.asc.gpg "$base_path"/gallery-dl/.config/gallery-dl/config.json
+gpg_decrypt_file "$data_path"/waybar/crypto/config.ini.asc.gpg "$base_path"/sway/.config/waybar/modules/crypto/config.ini
+gpg_decrypt_file "$data_path"/gtk/bookmarks.asc.gpg "$base_path"/gtk/.config/gtk-3.0/bookmarks
 
 stow -v ssh || {
     echo "Failed to stow ssh config"
@@ -379,13 +413,13 @@ stow -v nextcloud || {
 echo "Installing Mesa/Vulkan Drivers"
 yay_install mesa lib32-mesa mesa-vdpau lib32-mesa-vdpau libva-mesa-driver lib32-libva-mesa-driver libva-utils opencl-rusticl-mesa
 
-if [[ "$1" == "$laptop_name" ]]; then
+if [[ "$current_hostname" == "$laptop_hostname" ]]; then
     echo "Copying laptop system configuration"
 
-    gpg_list_dir "$DATA_PATH"/corectrl/laptop_profiles.gpgtar
-    gpg_decrypt_dir "$DATA_PATH"/corectrl/laptop_profiles.gpgtar "$BASE_PATH"
+    gpg_list_dir "$data_path"/corectrl/"$laptop_hostname"_profiles.gpgtar
+    gpg_decrypt_dir "$data_path"/corectrl/"$laptop_hostname"_profiles.gpgtar "$base_path"/corectrl/.config/corectrl
 
-    sudo rsync --chown=root:root --open-noatime --progress -ruav "$SYSTEM_CONFIG"/laptop/* /
+    sudo rsync --chown=root:root --open-noatime --progress -ruav "$system_config_path"/"$laptop_hostname"/* /
 
     echo "Installing Intel/Vulkan Drivers"
     yay_install xf86-video-intel vulkan-intel
@@ -411,7 +445,7 @@ if [[ "$1" == "$laptop_name" ]]; then
         exit 1
     }
 
-elif [[ "$1" == "$desktop_name" ]]; then
+elif [[ "$current_hostname" == "$desktop_hostname" ]]; then
     echo "Installing Radeon/Vulkan/ROCM drivers"
     yay_install xf86-video-amdgpu vulkan-radeon lib32-vulkan-radeon rocm-core rocm-opencl-runtime comgr hipblas rccl
 
@@ -428,15 +462,15 @@ elif [[ "$1" == "$desktop_name" ]]; then
         sudo grub-mkconfig -o /boot/grub/grub.cfg
     fi
 
-    gpg_list_dir "$DATA_PATH"/corectrl/workstation_profiles.gpgtar
-    gpg_decrypt_dir "$DATA_PATH"/corectrl/workstation_profiles.gpgtar "$BASE_PATH"
+    gpg_list_dir "$data_path"/corectrl/"$desktop_hostname"_profiles.gpgtar
+    gpg_decrypt_dir "$data_path"/corectrl/"$desktop_hostname"_profiles.gpgtar "$base_path"/corectrl/.config/corectrl
 
-    sudo rsync --chown=root:root --open-noatime --progress -ruav "$SYSTEM_CONFIG"/workstation/* /
+    sudo rsync --chown=root:root --open-noatime --progress -ruav "$system_config_path"/"$desktop_hostname"/* /
 
     echo "Installing liquidctl"
     yay_install liquidctl
 
-    symlink "$GIT_SUBMODULES"/liquidctl/extra/yoda.py "$BASE_PATH"/liquidctl/.local/bin/yoda
+    symlink "$git_submodule_path"/liquidctl/extra/yoda.py "$base_path"/liquidctl/.local/bin/yoda
 
     stow -v liquidctl || {
         echo "failed to stow liquidctl"
@@ -464,7 +498,7 @@ elif [[ "$1" == "$desktop_name" ]]; then
 fi
 
 echo "Copying common system configuration"
-sudo rsync --chown=root:root --open-noatime --progress -ruav "$SYSTEM_CONFIG"/common/* /
+sudo rsync --chown=root:root --open-noatime --progress -ruav "$system_config_path"/common/* /
 
 echo "Adding user to audio group"
 sudo groupadd audio || {
@@ -475,10 +509,10 @@ sudo usermod -a -G audio "$USER"
 echo "Copying themes from git repo to dotfiles locations"
 
 # If Hackneyed Dark build does not exist, then build it
-if [[ ! -d "$BASE_PATH"/gtk/.icons/hackneyed-dark ]] || [[ $hackneyed_updated == true ]]; then
+if [[ ! -d "$base_path"/gtk/.icons/hackneyed-dark ]] || [[ $hackneyed_updated == true ]]; then
     echo "Building hackneyed cursor"
 
-    cd "$GIT_SUBMODULES"/hackneyed-cursor || {
+    cd "$git_submodule_path"/hackneyed-cursor || {
         echo "failed to cd to hackneyed-cursor"
         exit 1
     }
@@ -493,45 +527,40 @@ if [[ ! -d "$BASE_PATH"/gtk/.icons/hackneyed-dark ]] || [[ $hackneyed_updated ==
         exit 1
     }
 
-    rmrf "$BASE_PATH"/gtk/.icons/hackneyed-dark
-    cp -rv "$GIT_SUBMODULES"/hackneyed-cursor/Hackneyed-Dark "$BASE_PATH"/gtk/.icons/hackneyed-dark # cp instead of ln because the build dir will get reset by git
+    rmrf "$base_path"/gtk/.icons/hackneyed-dark
+    cp -rv "$git_submodule_path"/hackneyed-cursor/Hackneyed-Dark "$base_path"/gtk/.icons/hackneyed-dark # cp instead of ln because the build dir will get reset by git
 
     make clean || {
         echo "failed to clean hackneyed make files"
         exit 1
     }
     rm -f ./*.tar.bz2
-    cd "$BASE_PATH" || {
-        echo "failed to cd to ${BASE_PATH}"
+    cd "$base_path" || {
+        echo "failed to cd to ${base_path}"
         exit 1
     }
 else
     echo "Skipping hackneyed cursor build"
 fi
 
-symlink "$GIT_SUBMODULES"/alacritty-theme/themes "$BASE_PATH"/alacritty/.config/alacritty/colors
+symlink "$git_submodule_path"/alacritty-theme/themes "$base_path"/alacritty/.config/alacritty/colors
 
-symlink "$GIT_SUBMODULES"/sweet-icons/Sweet-Purple "$BASE_PATH"/gtk/.icons/sweet-purple
+symlink "$git_submodule_path"/sweet-icons/Sweet-Purple "$base_path"/gtk/.icons/sweet-purple
 
-symlink "$GIT_SUBMODULES"/sweet-icons "$BASE_PATH"/gtk/.icons/sweet-icons
+symlink "$git_submodule_path"/sweet-icons "$base_path"/gtk/.icons/sweet-icons
 
-symlink "$GIT_SUBMODULES"/sweet-theme "$BASE_PATH"/gtk/.themes/sweet-theme
+symlink "$git_submodule_path"/sweet-theme "$base_path"/gtk/.themes/sweet-theme
 
-symlink "$GIT_SUBMODULES"/candy-icons "$BASE_PATH"/gtk/.icons/candy-icons
+symlink "$git_submodule_path"/candy-icons "$base_path"/gtk/.icons/candy-icons
 
-symlink "$GIT_SUBMODULES"/buuf-nestort-icons "$BASE_PATH"/gtk/.icons/buuf-nestort-icons
+symlink "$git_submodule_path"/buuf-nestort-icons "$base_path"/gtk/.icons/buuf-nestort-icons
 
-symlink "$GIT_SUBMODULES"/catppuccin-bat/themes/Catppuccin\ Mocha.tmTheme "$BASE_PATH"/bat/.config/bat/themes/Catppuccin-Mocha.tmTheme
+symlink "$git_submodule_path"/catppuccin-bat/themes/Catppuccin\ Mocha.tmTheme "$base_path"/bat/.config/bat/themes/Catppuccin-Mocha.tmTheme
 
-rmrf "$BASE_PATH"/gtk/.themes/materia-cyberpunk-neon
-unzip -o "$GIT_SUBMODULES"/cyberpunk-theme/gtk/materia-cyberpunk-neon.zip -d "$BASE_PATH"/gtk/.themes || {
+rmrf "$base_path"/gtk/.themes/materia-cyberpunk-neon
+unzip -o "$git_submodule_path"/cyberpunk-theme/gtk/materia-cyberpunk-neon.zip -d "$base_path"/gtk/.themes || {
     echo "failed copying Cyberpunk-Neon theme"
     exit 1
-}
-
-echo "Checking for old dependencies to remove"
-yay -R --noconfirm swaylock swaylock-blur pipewire-media-session pipewire-pulseaudio pipewire-pulseaudio-git pulseaudio-equalizer pulseaudio-lirc pulseaudio-zeroconf pulseaudio pulseaudio-bluetooth redshift-wayland-git birdtray alacritty-colorscheme ly || {
-    echo "no old dependencies found"
 }
 
 echo "Installing Python dependencies"
@@ -554,7 +583,36 @@ rustup component add clippy rustfmt || {
 }
 
 echo "Installing node.js toolchain"
-yay_install nodejs npm nvm electron
+yay_install nodejs npm electron nvm-git
+
+nvm_init_script="source /usr/share/nvm/init-nvm.sh"
+if [[ $(line_exists "$nvm_init_script" ~/.bashrc) == 1 ]]; then
+    echo "Adding NVM init script to ~/.bashrc"
+    echo 'source /usr/share/nvm/init-nvm.sh' >>~/.bashrc
+fi
+
+if [[ $(line_exists "$nvm_init_script" ~/.zshrc) == 1 ]]; then
+    echo "Adding NVM init script to ~/.zshrc"
+    echo 'source /usr/share/nvm/init-nvm.sh' >>~/.zshrc
+fi
+
+source ~/.bashrc
+
+nvm install lts/* || {
+    echo "failed to install Node LTS"
+    exit 1
+}
+
+nvm alias default lts/* || {
+    echo "failed to set node default version to LTS"
+    exit 1
+}
+
+echo "Installing Java dependencies"
+yay_install jdk-openjdk || {
+    echo "Failed to install Java dependencies"
+    exit 1
+}
 
 echo "Installing QT Dependencies"
 yay_install qt5-wayland qt6-wayland qtkeychain-qt5 qtkeychain-qt6 qgnomeplatform-qt5 qgnomeplatform-qt6
@@ -574,10 +632,10 @@ echo "Installing Pipewire dependencies"
 yay_install pipewire pipewire-pulse pipewire-alsa wireplumber alsa-tools
 
 echo "Installing Bluetooth dependencies"
-yay_install bluez bluez-utils bluez-obex bluetuith-bin
+yay_install bluez bluez-utils bluez-obex bluetuith-git
 
 echo "Checking for general utilities dependencies to install"
-yay_install gvfs gvfs-smb thunar thunar-volman thunar-shares-plugin tumbler mpv smartmontools batsignal mimeo htop udiskie pavucontrol wdisplays ranger shotwell rbw light mako alacritty gnome-keyring cava iniparser fftw bemenu-wayland pinentry-bemenu libnotify kanshi helvum xdg-desktop-portal xdg-desktop-portal-wlr wayland-protocols dex gammastep geoclue lxappearance otf-font-awesome ttf-hack dust okular gallery-dl-git bat nextcloud-client
+yay_install gvfs gvfs-smb thunar thunar-volman thunar-shares-plugin tumbler mpv smartmontools batsignal mimeo htop udiskie pavucontrol wdisplays ranger shotwell rbw light mako alacritty gnome-keyring cava iniparser fftw bemenu-wayland pinentry-bemenu libnotify kanshi helvum xdg-desktop-portal xdg-desktop-portal-wlr wayland-protocols dex gammastep geoclue lxappearance otf-font-awesome ttf-hack dust okular gallery-dl-git bat nextcloud-client opensnitch hopenpgp-tools ddrescue nmap
 
 bat cache --build || {
     echo "failed to build bat cache"
@@ -628,8 +686,8 @@ yay -R --noconfirm vim || {
     echo "vim not installed, skipping removal"
 }
 
-echo "Installing  neovim"
-yay_install neovim python-pynvim neovim-symlinks
+echo "Installing neovim"
+yay_install neovim python-pynvim neovim-symlinks tinyxxd
 
 echo "Installing PlatformIO"
 yay_install platformio-core platformio-core-udev
@@ -638,7 +696,7 @@ echo "Installing qbitorrent"
 yay_install qbittorrent
 
 echo "Installing freetube"
-yay_install freetube-bin
+yay_install freetube-git
 
 echo "Installing Solaar (Logitech manager)"
 yay_install solaar
@@ -646,32 +704,29 @@ yay_install solaar
 echo "Installing TIDAL-HiFi"
 yay_install tidal-hifi-git
 
-echo "Installing Brave"
-yay_install brave-bin
-
-# echo "Installing Conda/Micromamba"
-# yay_install miniconda3 conda-zsh-completion micromamba-bin
+echo "Installing Conda tooling"
+yay_install rattler-build
 
 echo "Installing VSCodium"
-yay_install vscodium-bin vscodium-bin-features vscodium-bin-marketplace
+yay_install vscodium-git-wayland-hook vscodium-git vscodium-git-features vscodium-git-marketplace
 
 echo "Installing Betterbird dependencies"
 yay_install betterbird-bin
 
 echo "Installing Video Editing dependencies"
-yay_install gyroflow-bin kdenlive
+yay_install gyroflow-git kdenlive
 
 echo "Installing Logseq dependencies"
-yay_install logseq-desktop-bin
+yay_install logseq-desktop-git
 
 echo "Installing Nextcloud dependencies"
 yay_install nextcloud-client
 
 echo "Installing Steam dependencies"
-yay_install steam-native-runtime
+yay_install steam-native-runtime steamtinkerlaunch-git
 
-echo "Installing GPG / YubiKey dependencies"
-yay_install gnupg seahorse pcsclite ccid hopenpgp-tools yubikey-personalization yubikey-manager
+echo "Installing YubiKey dependencies"
+yay_install yubikey-personalization yubikey-manager yubikey-manager-qt
 
 echo "Disabling GNOME Keyring SSH Agent"
 
--- store_secrets.sh
+++ store_secrets.sh
@@ -3,14 +3,16 @@
 GPG_SIGNING_KEY=0xA6C9F349967F1AC6
 RECIPIENT_GPG_ENCRYPTION_KEY=0x79C70BBE4865D828
 
-data_dir="./data"
+base_path=$PWD
+data_path="$base_path"/data
 
-laptop_name="laptop"
-desktop_name="workstation"
+laptop_hostname="thinky"
+desktop_hostname="shifty"
+current_hostname=$(hostname)
 
 exit_script() {
     rv=$?
-    echo "Exiting..."
+    printf "\n\nExiting...\n"
     exit $rv
 }
 
@@ -19,7 +21,7 @@ trap 'exit_script' ERR INT
 
 script_name=$(basename "$0")
 help() {
-    printf "Stores configuration secrets to \"%s\".\n\nUsage: %s <laptop|workstaton>\n" "$data_dir" "$script_name"
+    printf "Stores configuration secrets to \"%s\".\n\nUsage: %s <%s|%s>\n" "$data_path" "$script_name" "$laptop_hostname" "$desktop_hostname"
 }
 
 gpg_encrypt_file() {
@@ -27,33 +29,36 @@ gpg_encrypt_file() {
 }
 
 gpg_encrypt_dir() {
-    gpgtar -v --gpg-args "--local-user ${GPG_SIGNING_KEY} --recipient ${RECIPIENT_GPG_ENCRYPTION_KEY}" --sign --output "${2}" --encrypt "${1}"
+    gpgtar -v --gpg-args "--local-user ${GPG_SIGNING_KEY} --recipient ${RECIPIENT_GPG_ENCRYPTION_KEY} --yes" --sign --directory "${1}" --output "${3}" --encrypt "${2}"
 }
 
-if [ "$#" -ne 1 ]; then
-    help
-    exit 0
+if [[ "$current_hostname" != "$laptop_hostname" ]] && [[ "$current_hostname" != "$desktop_hostname" ]]; then
+    if [[ "$1" != "$laptop_hostname" ]] && [[ "$1" != "$desktop_hostname" ]]; then
+        echo "Unrecognised hostname! Please provide a valid one."
+        help
+        exit 1
+    else
+        echo "Unrecognised hostname. Falling back to provided parameter: \"${1}\"."
+        current_hostname="$1"
+    fi
+else
+    echo "Using current hostname \"${current_hostname}\" for configuration."
 fi
 
-if [[ "$1" != "$laptop_name" ]] && [[ "$1" != "$desktop_name" ]]; then
-    help
-    exit 1
-fi
-
-gpg --import "$data_dir"/gpg/2B7340DB13C85766.asc
+gpg --import "$data_path"/gpg/2B7340DB13C85766.asc
 
 # Shared Files
-gpg_encrypt_file ~/.ssh/config "$data_dir"/ssh/config.asc.gpg
-gpg_encrypt_file ~/.config/mimeapps.list "$data_dir"/xdg/mimeapps.list.asc.gpg
-gpg_encrypt_file ~/.config/tidal-hifi/config.json "$data_dir"/tidal-hifi/config.json.asc.gpg
-gpg_encrypt_file ~/.config/gallery-dl/config.json "$data_dir"/gallery-dl/config.json.asc.gpg
-gpg_encrypt_file ~/.config/waybar/modules/crypto/config.ini "$data_dir"/waybar/crypto/config.ini.asc.gpg
-gpg_encrypt_file ~/.config/gtk-3.0/bookmarks "$data_dir"/gtk/bookmarks.asc.gpg
+gpg_encrypt_file ~/.ssh/config "$data_path"/ssh/config.asc.gpg
+gpg_encrypt_file ~/.config/mimeapps.list "$data_path"/xdg/mimeapps.list.asc.gpg
+gpg_encrypt_file ~/.config/tidal-hifi/config.json "$data_path"/tidal-hifi/config.json.asc.gpg
+gpg_encrypt_file ~/.config/gallery-dl/config.json "$data_path"/gallery-dl/config.json.asc.gpg
+gpg_encrypt_file ~/.config/waybar/modules/crypto/config.ini "$data_path"/waybar/crypto/config.ini.asc.gpg
+gpg_encrypt_file ~/.config/gtk-3.0/bookmarks "$data_path"/gtk/bookmarks.asc.gpg
 
 # Hardware-Specific Files
-if [[ "$1" == "$laptop_name" ]]; then
-    gpg_encrypt_dir ~/.config/corectrl/profiles "$data_dir"/corectrl/laptop_profiles.gpgtar
-elif [[ "$1" == "$desktop_name" ]]; then
-    gpg_encrypt_dir ~/.config/corectrl/profiles "$data_dir"/corectrl/workstation_profiles.gpgtar
+if [[ "$current_hostname" == "$laptop_hostname" ]]; then
+    gpg_encrypt_dir ~/.config/corectrl profiles/ "$data_path"/corectrl/"$laptop_hostname"_profiles.gpgtar
+elif [[ "$current_hostname" == "$desktop_hostname" ]]; then
+    gpg_encrypt_dir ~/.config/corectrl profiles/ "$data_path"/corectrl/"$desktop_hostname"_profiles.gpgtar
 
 fi
