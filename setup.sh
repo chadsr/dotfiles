5a1186fe11e92fdf5a55dd5c58745a73ac6a7f77
Ross <git@ross.ch>
2025-04-02 14:45:16 +0200

    steam: symlink instead of stow

--- setup.sh
+++ setup.sh
@@ -845,6 +845,7 @@ declare -a symlink_paths_tuples=(
     "${git_submodule_path}/sweet-theme/gtk-4.0 ${base_path}/gtk/.themes/Sweet/gtk-4.0"
     "${git_submodule_path}/sweet-theme/index.theme ${base_path}/gtk/.themes/Sweet/index.theme"
     "${git_submodule_path}/waybar-crypto/.submodules/cryptofont/fonts/cryptofont.ttf ${base_path}/waybar/.local/share/fonts/TTF/cryptofont.ttf"
+    "${base_path}/steam/.steam/steam/steam_dev.cfg ${HOME}/.steam/steam/steam_dev.cfg"
 )
 for symlink_paths_tuple in "${symlink_paths_tuples[@]}"; do
     read -ra symlink_paths <<<"$symlink_paths_tuple"
@@ -888,7 +889,6 @@ declare -a stow_dirs_general=(
     qt
     ranger
     scripts
-    steam
     sway
     swww
     systemd
