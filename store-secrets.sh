dfde221ad89040c7d1a998ccff185ce2c937baa0
Ross <git@ross.ch>
2026-03-09 16:33:53 +0100

    nextcloud: init cfg

--- store-secrets.sh
+++ store-secrets.sh
@@ -109,6 +109,7 @@ declare -a encrypt_data_paths_tuples=(
     "${HOME}/.config/gtk-3.0/bookmarks ${data_path}/gtk/bookmarks.asc.gpg"
     "${HOME}/.config/khal/config ${data_path}/khal/config.asc.gpg"
     "${HOME}/.config/mimeapps.list ${data_path}/xdg/mimeapps.list.asc.gpg"
+    "${HOME}/.config/Nextcloud/nextcloud.cfg ${data_path}/nextcloud/nextcloud.cfg.asc.gpg"
     "${HOME}/.config/PrusaSlicer/PrusaSlicer.ini ${data_path}/prusaslicer/PrusaSlicer.ini.asc.gpg"
     "${HOME}/.config/qBittorrent/qBittorrent.conf ${data_path}/qbittorrent/categories.json.asc"
     "${HOME}/.config/qBittorrent/qBittorrent.conf ${data_path}/qbittorrent/qBittorrent.conf.asc"
@@ -121,7 +122,6 @@ declare -a encrypt_data_paths_tuples=(
     "$base_path/system/${current_hostname}/boot/loader/entries/arch-cachyos.conf ${data_path}/system/${current_hostname}/boot/loader/entries/arch-cachyos.asc.conf"
     "$base_path/system/${current_hostname}/boot/loader/entries/arch-zen.conf ${data_path}/system/${current_hostname}/boot/loader/entries/arch-zen.asc.conf"
     "$data_path/pkgs/remove.txt ${data_path}/pkgs/remove.txt.asc.gpg"
-
 )
 
 for encrypt_data_paths_tuple in "${encrypt_data_paths_tuples[@]}"; do
